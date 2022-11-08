febd785a870075db118fda240bb486ec7c06060f
Livier <572440312@qq.com>
2022-11-08 16:11:55 +0800

    过滤器，检查是否完成登录

---
+++ Reggie_Takeout/src/main/java/com/lwa/filter/LoginCheckFilter.java
@@ -0,0 +1,82 @@
+package com.lwa.filter;
+
+import com.alibaba.fastjson.JSON;
+import com.lwa.common.R;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.util.AntPathMatcher;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * 检查用户是否已经完成登录
+ */
+@WebFilter(filterName = "loginCheckFilter",urlPatterns = "/*") //"/*"所有请求都拦截
+@Slf4j
+public class LoginCheckFilter implements Filter {
+    //路径匹配器，支持通配符
+    public static final AntPathMatcher PATH_MATCHER = new AntPathMatcher();
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+
+        /**
+         * 1、获取本次请求的URI（统一资源标识符，用于标识某一互联网资源名称的字符串）
+         * 2、判断本次请求是否需要处理
+         * 3、如果不需要处理则直接放行
+         * 4、判断登录状态，如果已登陆，则直接放行
+         * 5、如果未登录则返回未登录结果
+         */
+
+        //1、获取本次请求的URI
+        String requestURI = request.getRequestURI();
+        //定义不需要处理的请求路径，直接放行
+        String[] urls = new String[]{
+                "/employee/login",//登录
+                "/employee/logout",//退出
+                "/backend/**",
+                "/front/**"
+        };
+
+        //2、判断本次请求是否需要处理
+        boolean check = check(urls, requestURI);
+
+        //3、如果不需要处理则直接放行
+        if(check){
+            filterChain.doFilter(request,response);
+            return;
+        }
+
+        //4、判断登录状态，如果已登陆，则直接放行
+        if(request.getSession().getAttribute("employee")!=null){
+            filterChain.doFilter(request,response);
+            return;
+        }
+
+        //5、如果未登录则返回未登录结果，通过输出流方式向客户端页面响应数据
+        response.getWriter().write(JSON.toJSONString(R.error("NOTLOGIN")));
+        return;
+    }
+
+    /**
+     * 路径匹配，检查本次请求是否需要放行
+     * @param urls
+     * @param requestURI
+     * @return
+     */
+    public boolean check(String[] urls, String requestURI){
+        //判断urls数组中的每一个元素url是否为true
+        for (String url : urls) {
+            boolean match = PATH_MATCHER.match(url,requestURI);
+            if(match){
+                return true;
+            }
+        }
+        return false;
+    }
+}
